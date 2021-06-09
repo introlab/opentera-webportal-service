c2602f6f94ee5328baf8a8489a8147dd85395acd
cvilanova <cyvila@gmail.com>
2021-06-08 21:07:23 -0400

    angular 12

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {environment} from '../../../environments/environment';
+import {environment} from '@src/environments/environment';
 import {GlobalConstants} from '@core/utils/global-constants';
 
 @Component({
