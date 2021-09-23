3fb90ab08abcef2c6bfd689cdbbf653b2778cf99
cvilanova <cyvila@gmail.com>
2021-09-23 14:46:20 -0400

    app layout edit

--- src/app/shared/layout/app-layout/app-layout.component.ts
+++ src/app/shared/layout/app-layout/app-layout.component.ts
@@ -1,8 +1,15 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ShowResponsiveNavigationService} from '@services/show-responsive-navigation.service';
-import {Subscription} from 'rxjs';
+import {combineLatest, Subscription} from 'rxjs';
 import {AccountService} from '@services/account.service';
 import {Account} from '@shared/models/account.model';
+import {SelectedSiteService} from '@services/selected-site.service';
+import {SelectedProjectService} from '@services/selected-project.service';
+import {Site} from '@shared/models/site.model';
+import {Project} from '@shared/models/project.model';
+import {isObjectEmpty, isUser} from '@core/utils/utility-functions';
+import {ServiceService} from '@services/service.service';
+import {filter, switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-app-layout',
@@ -11,22 +18,58 @@ import {Account} from '@shared/models/account.model';
 })
 export class AppLayoutComponent implements OnInit, OnDestroy {
   navigationOpened = false;
-  private subscription!: Subscription;
-  private account!: Account;
+  account: Account;
+  selectedSite: Site;
+  selectedProject: Project;
+  isUser = false;
+  showSelectors = false;
+  showBackButtonSelectors = false;
+  private subscriptions: Subscription[] = [];
 
   constructor(private showNavService: ShowResponsiveNavigationService,
-              private accountService: AccountService) {
+              private accountService: AccountService,
+              private serviceService: ServiceService,
+              private selectedSiteService: SelectedSiteService,
+              private selectedProjectService: SelectedProjectService) {
   }
 
   ngOnInit(): void {
-    const showNav$ = this.showNavService.showNavigation();
+    this.getService();
+    this.getSessionData();
+  }
 
-    this.subscription = showNav$.subscribe((showNav) => {
-      this.navigationOpened = showNav;
-    });
+  private getService(): void {
+    const account$ = this.accountService.account$();
+    this.subscriptions.push(
+      account$.pipe(
+        tap((account) => this.account = account),
+        filter((account) => isUser(account)),
+        switchMap(() => this.serviceService.getByKey())
+      ).subscribe((res) => console.log(res))
+    );
+  }
+
+  private getSessionData(): void {
+    const account$ = this.accountService.account$();
+    const showNav$ = this.showNavService.showNavigation();
+    const selectedSite$ = this.selectedSiteService.getSelectedSite();
+    const selectedProject$ = this.selectedProjectService.getSelectedProject();
+
+    this.subscriptions.push(
+      combineLatest([account$, showNav$, selectedSite$, selectedProject$])
+        .subscribe(([account, showNav, selectedSite, selectedProject]) => {
+          this.account = account;
+          this.navigationOpened = showNav;
+          this.selectedSite = selectedSite;
+          this.selectedProject = selectedProject;
+          this.isUser = isUser(account);
+          this.showBackButtonSelectors = !isObjectEmpty(selectedSite) && !isObjectEmpty(selectedProject);
+          this.showSelectors = this.isUser && this.showBackButtonSelectors && this.selectedProject.id_site === this.selectedSite.id_site;
+        })
+    );
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
