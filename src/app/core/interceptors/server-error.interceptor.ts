29f1ba10091ad6dd4d88356339e271488223df4e
cvilanova <cyvila@gmail.com>
2021-09-23 14:48:41 -0400

    delete cookie if null

--- src/app/core/interceptors/server-error.interceptor.ts
+++ src/app/core/interceptors/server-error.interceptor.ts
@@ -7,6 +7,7 @@ import {AuthenticationService} from '@services/authentication.service';
 import {LoginButtonService} from '@services/login-button.service';
 import {NotificationService} from '@services/notification.service';
 import {GlobalConstants} from '@core/utils/global-constants';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable()
 export class ServerErrorInterceptor implements HttpInterceptor {
@@ -20,6 +21,7 @@ export class ServerErrorInterceptor implements HttpInterceptor {
     const authService = this.injector.get(AuthenticationService);
     const notifier = this.injector.get(NotificationService);
     const loginButton = this.injector.get(LoginButtonService);
+    const cookieService = this.injector.get(CookieService);
 
     return next.handle(request).pipe(
       retry(1),
@@ -27,6 +29,9 @@ export class ServerErrorInterceptor implements HttpInterceptor {
         if (error.url && error.url.includes('/login')) {
           loginButton.enableButton(false);
           notifier.showError('Mauvais nom d\'utilisateur ou mot de passe.');
+        }
+        if (!cookieService.get(GlobalConstants.cookieValue)) {
+          cookieService.delete(GlobalConstants.cookieValue);
         }
         if (error.status === 401) {
           authService.logout().subscribe();
