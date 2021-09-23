b303d2ac4697008bfda4657832c0cf6db32626f0
cvilanova <cyvila@gmail.com>
2021-09-23 14:43:23 -0400

    Create icons.ts

---
+++ src/app/core/utils/icons.ts
@@ -0,0 +1,334 @@
+import {Icon} from '@shared/models/icon.model';
+
+export const icons: Icon[] = [
+  {
+    code: 'search'
+  },
+  {
+    code: 'settings'
+  },
+  {
+    code: 'info'
+  },
+  {
+    code: 'shopping_cart'
+  },
+  {
+    code: 'visibility'
+  },
+  {
+    code: 'favorite'
+  },
+  {
+    code: 'description'
+  },
+  {
+    code: 'lock'
+  },
+  {
+    code: 'face'
+  },
+  {
+    code: 'fingerprint'
+  },
+  {
+    code: 'schedule'
+  },
+  {
+    code: 'language'
+  },
+  {
+    code: 'help_outline'
+  },
+  {
+    code: 'manage_accounts'
+  },
+  {
+    code: 'filter_alt'
+  },
+  {
+    code: 'event'
+  },
+  {
+    code: 'dashboard'
+  },
+  {
+    code: 'calendar_today'
+  },
+  {
+    code: 'list'
+  },
+  {
+    code: 'visibility_off'
+  },
+  {
+    code: 'date_range'
+  },
+  {
+    code: 'account_balance'
+  },
+  {
+    code: 'trending_up'
+  },
+  {
+    code: 'shopping_bag'
+  },
+  {
+    code: 'lightbulb'
+  },
+  {
+    code: 'paid'
+  },
+  {
+    code: 'question_answer'
+  },
+  {
+    code: 'article'
+  },
+  {
+    code: 'credit_card'
+  },
+  {
+    code: 'history'
+  },
+  {
+    code: 'fact_check'
+  },
+  {
+    code: 'assignment'
+  },
+  {
+    code: 'report_problem'
+  },
+  {
+    code: 'star_rate'
+  },
+  {
+    code: 'verified_user'
+  },
+  {
+    code: 'build'
+  },
+  {
+    code: 'account_balance_wallet'
+  },
+  {
+    code: 'autorenew'
+  },
+  {
+    code: 'view_list'
+  },
+  {
+    code: 'print'
+  },
+  {
+    code: 'analytics'
+  },
+  {
+    code: 'work'
+  },
+  {
+    code: 'savings'
+  },
+  {
+    code: 'store'
+  },
+  {
+    code: 'today'
+  },
+  {
+    code: 'admin_panel_settings'
+  },
+  {
+    code: 'room'
+  },
+  {
+    code: 'pets'
+  },
+  {
+    code: 'update'
+  },
+  {
+    code: 'watch_later'
+  },
+  {
+    code: 'power_settings_new'
+  },
+  {
+    code: 'receipt'
+  },
+  {
+    code: 'explore'
+  },
+  {
+    code: 'bookmark'
+  },
+  {
+    code: 'account_box'
+  },
+  {
+    code: 'shopping_basket'
+  },
+  {
+    code: 'reorder'
+  },
+  {
+    code: 'pending_actions'
+  },
+  {
+    code: 'payment'
+  },
+  {
+    code: 'drag_indicator'
+  },
+  {
+    code: 'leaderboard'
+  },
+  {
+    code: 'touch_app'
+  },
+  {
+    code: 'supervisor_account'
+  },
+  {
+    code: 'assessment'
+  },
+  {
+    code: 'card_giftcard'
+  },
+  {
+    code: 'pan_tool'
+  },
+  {
+    code: 'accessibility'
+  },
+  {
+    code: 'timeline'
+  },
+  {
+    code: 'bug_report'
+  },
+  {
+    code: 'book'
+  },
+  {
+    code: 'alarm'
+  },
+  {
+    code: 'contact_page'
+  },
+  {
+    code: 'record_voice_over'
+  },
+  {
+    code: 'accessibility_new'
+  },
+  {
+    code: 'hourglass_empty'
+  },
+  {
+    code: 'sticky_note_2'
+  },
+  {
+    code: 'tips_and_updates'
+  },
+  {
+    code: 'sensors'
+  },
+  {
+    code: 'pregnant_woman'
+  },
+  {
+    code: 'commute'
+  },
+  {
+    code: 'maps_home_work'
+  },
+  {
+    code: 'mail'
+  },
+  {
+    code: 'save'
+  },
+  {
+    code: 'send'
+  },
+  {
+    code: 'inventory'
+  },
+  {
+    code: 'bolt'
+  },
+  {
+    code: 'create'
+  },
+  {
+    code: 'flag'
+  },
+  {
+    code: 'push_pin'
+  },
+  {
+    code: 'calculate'
+  },
+  {
+    code: 'content_paste'
+  },
+  {
+    code: 'report'
+  },
+  {
+    code: 'save_alt'
+  },
+  {
+    code: 'notifications'
+  },
+  {
+    code: 'school'
+  },
+  {
+    code: 'emoji_events'
+  },
+  {
+    code: 'people'
+  },
+  {
+    code: 'public'
+  },
+  {
+    code: 'social_distance'
+  },
+  {
+    code: 'elderly'
+  },
+  {
+    code: 'follow_the_signs'
+  },
+  {
+    code: 'emoji_transportation'
+  },
+  {
+    code: 'clean_hands'
+  },
+  {
+    code: 'real_estate_agent'
+  },
+  {
+    code: 'hiking'
+  },
+  {
+    code: 'luggage'
+  },
+  {
+    code: 'cake'
+  },
+  {
+    code: 'photo_camera'
+  },
+  {
+    code: 'palette'
+  },
+  {
+    code: 'auto_stories'
+  },
+];
