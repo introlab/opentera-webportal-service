75d97539684bd7b287bd15cf31ddb5f3bfbd3e79
cvilanova <cyvila@gmail.com>
2021-09-23 14:42:34 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
 import {AuthenticationService} from '@services/authentication.service';
-import {GlobalConstants} from '@core/utils/global-constants';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent} from '@angular/router';
 import {delay} from 'rxjs/operators';
+import {AccountService} from '@services/account.service';
+import {GlobalConstants} from '@core/utils/global-constants';
+import {Subscription} from 'rxjs';
+import {isUser} from '@core/utils/utility-functions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Promo Santé';
   loading: boolean;
+  private subscription: Subscription;
 
   constructor(private cookieService: CookieService,
               private authService: AuthenticationService,
+              private accountService: AccountService,
               private router: Router) {
   }
 
@@ -25,9 +30,16 @@ export class AppComponent implements OnInit {
         this.navigationInterceptor(e);
       }
     });
-    if (!!this.cookieService.get(GlobalConstants.cookieValue)) {
-      this.authService.startRefreshTokenTimer();
-    }
+    //this.refreshToken();
+  }
+
+  private refreshToken(): void {
+    this.subscription = this.accountService.account$().subscribe((account) => {
+      if (isUser(account) && !!this.cookieService.get(GlobalConstants.cookieValue)) {
+        console.log('refresh token user');
+        this.authService.startRefreshTokenTimer();
+      }
+    });
   }
 
   // Shows and hides the loading spinner during RouterEvent changes
@@ -37,5 +49,9 @@ export class AppComponent implements OnInit {
     } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
       this.loading = false;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
