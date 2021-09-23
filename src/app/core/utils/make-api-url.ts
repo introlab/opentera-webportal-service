817e7b7d9ec11180bc31e9332fdabbfc9b0228a3
cvilanova <cyvila@gmail.com>
2021-09-23 14:43:45 -0400

    utility functions

--- src/app/core/utils/make-api-url.ts
+++ src/app/core/utils/make-api-url.ts
@@ -1,6 +1,6 @@
 import {GlobalConstants} from './global-constants';
 
-export function makeApiURL(forPSService = false) {
+export function makeApiURL(forPSService = false): string {
   const url = GlobalConstants.protocol + '://' + GlobalConstants.hostname + ':' + GlobalConstants.port + '/';
   if (forPSService) {
     return url + GlobalConstants.psService + GlobalConstants.osService;
--- src/app/core/utils/utility-functions.ts
+++ src/app/core/utils/utility-functions.ts
@@ -1,4 +1,5 @@
 import {Account} from '@shared/models/account.model';
+import {GlobalConstants} from '@core/utils/global-constants';
 
 export function dateToISOLikeButLocal(date: Date): string {
   const offsetMs = date.getTimezoneOffset() * 60 * 1000;
@@ -13,13 +14,31 @@ export function setDate(date: Date, time: Date): Date {
   return date;
 }
 
-export function getDuration(startTime: Date, endTime: Date, date: Date): number {
-  const start = setDate(date, startTime);
-  const end = setDate(date, endTime);
-  const diffMs = end.getTime() - start.getTime();
+export function getDuration(startTime: Date, endTime: Date): number {
+  const diffMs = endTime.getTime() - startTime.getTime();
   return Math.floor(diffMs / 60000);
 }
 
 export function isUser(account: Account): boolean {
   return account.login_type === 'user';
+}
+
+export function convertMinutesToHoursMinutes(minutes: number): string {
+  const h = Math.floor(minutes / 60);
+  const m = minutes % 60;
+  if (minutes < 60) {
+    return m + 'min';
+  } else {
+    const mString = m < 10 ? '0' + m : m;
+    return h + 'h' + mString;
+  }
+}
+
+export function isObjectEmpty(object: any): boolean {
+  return !object || !Object.keys(object).length;
+}
+
+// https://(url du serveur):(port)/webportal/participant?token=(token du participant))
+export function createParticipantUrl(token: string): string {
+  return `${GlobalConstants.protocol}://${GlobalConstants.hostname}:${GlobalConstants.port}/webportal/participant?token=${token}`;
 }
