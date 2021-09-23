ef60f15c8e18954db4dd5331e2540fef94e01830
cvilanova <cyvila@gmail.com>
2021-09-23 14:41:52 -0400

    constants for paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,35 @@ import {AppLayoutComponent} from '@shared/layout/app-layout/app-layout.component
 import {LoginLayoutComponent} from '@shared/layout/login-layout/login-layout.component';
 import {LoginComponent} from '@pages/login/login.component';
 import {AuthGuardService} from '@services/guards/auth-guard.service';
-import {CalendarComponent} from '@pages/calendar/calendar.component';
+import {CalendarPageComponent} from '@pages/calendar/calendar.component';
 import {NotFoundComponent} from '@pages/not-found/not-found.component';
-import {EmailsComponent} from '@pages/emails/emails.component';
-import {SeanceComponent} from '@pages/seance/seance.component';
-import {ExercisesComponent} from '@pages/exercices/exercises.component';
+import {EmailsComponent} from '@pages/participant/emails/emails.component';
+import {SeanceComponent} from '@pages/participant/seance/seance.component';
+import {ExercisesComponent} from '@pages/participant/exercices/exercises.component';
 import {AccountResolver} from '@services/resolvers/account.resolver';
 import {CallbackComponent} from '@components/callback/callback.component';
 import {GlobalConstants} from '@core/utils/global-constants';
+import {ServiceResolver} from '@services/resolvers/service.resolver';
+import {EventFormComponent} from '@pages/event-form/event-form.component';
+import {ParticipantsComponent} from '@pages/user/participants/participants.component';
+import {ApplicationsComponent} from '@pages/user/applications/applications.component';
+import {PlanningComponent} from '@pages/user/planning/planning.component';
 
 const routes: Routes = [
   {path: '', redirectTo: GlobalConstants.calendarPage, pathMatch: 'full'},
   {path: 'rehab/participant', component: CallbackComponent},
+  {path: 'rehab/user', component: CallbackComponent},
   {
     path: '',
     component: AppLayoutComponent,
     canActivate: [AuthGuardService],
     resolve: [AccountResolver],
     children: [
-      {path: GlobalConstants.calendarPage, component: CalendarComponent},
+      {path: GlobalConstants.calendarPage, component: CalendarPageComponent},
+      {path: GlobalConstants.participantsPage, component: ParticipantsComponent},
+      {path: GlobalConstants.planningPage, component: PlanningComponent},
+      {path: GlobalConstants.applicationsPage, component: ApplicationsComponent},
+      {path: GlobalConstants.eventFormPage, component: EventFormComponent},
       {path: GlobalConstants.emailPage, component: EmailsComponent},
       {path: GlobalConstants.sessionPage, component: SeanceComponent},
       {path: GlobalConstants.exercisesPage, component: ExercisesComponent},
--- src/app/core/utils/global-constants.ts
+++ src/app/core/utils/global-constants.ts
@@ -6,13 +6,21 @@ export class GlobalConstants {
   static osService = 'api/';
   static psService = 'webportal/';
   static cookieValue = 'PromoSanteCookie';
+  static serviceKey = 'WebPortalService';
   static version = '1.0';
   static organism = 'Centre de Recherche sur le Vieillissement de Sherbrooke - Regroupement INTER - 2020';
+
+  static applicationTypes = {external: 0, 'OpenTera Service': 1};
 
   // App page urls
   static homePage = '';
   static loginPage = 'connexion';
   static calendarPage = 'calendrier';
+  static eventFormPage = 'evenement';
+  static participantsPage = 'participants';
+  static sectionsPage = 'sections';
+  static planningPage = 'planifiation';
+  static applicationsPage = 'sections';
   static sessionPage = 'seance';
   static emailPage = 'courriel';
   static exercisesPage = 'exercices';
@@ -22,4 +30,7 @@ export class GlobalConstants {
   static emailLink = 'https://mail.google.com/';
   static seanceLink = 'https://telesante.cdrv.ca:40001?webId=ihkLMOuLvzYdpE2QEcX435kn';
   static physiotecLink = 'https://physiotec.ca/ca/en/login/';
+
+  // Strings
+  static requiredMessage = 'Ce champ est requis.';
 }
