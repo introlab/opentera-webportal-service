21205ced211256f273025c85f751964a7a2c2d61
cvilanova <cyvila@gmail.com>
2021-09-29 12:50:08 -0400

    connect to session in video rehab page & live indicator

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -10,6 +10,7 @@ import {Pages} from '@core/utils/pages';
 import {AccountService} from '@services/account.service';
 import {Subscription} from 'rxjs';
 import {Account} from '@shared/models/account.model';
+import {SelectedSourceService} from '@services/selected-source.service';
 
 @Component({
   selector: 'app-event-card',
@@ -20,7 +21,7 @@ export class EventCardComponent implements OnInit, OnDestroy {
   @Input() event: Event;
   duration: string;
   isUser = false;
-  isEventToCome = false;
+  isLive = false;
   isPastEvent = true;
   private subscription: Subscription;
   private account: Account;
@@ -28,17 +29,26 @@ export class EventCardComponent implements OnInit, OnDestroy {
   constructor(private calendarService: CalendarService,
               private router: Router,
               private accountService: AccountService,
+              private selectedSourceService: SelectedSourceService,
               private notificationService: NotificationService,
               public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+    this.checkIfEventIsLive();
     this.checkIfEventIsPassed();
     this.getDuration();
     this.subscription = this.accountService.account$().subscribe((account) => {
       this.account = account;
       this.isUser = isUser(account);
     });
+  }
+
+  private checkIfEventIsLive(): void {
+    const now = new Date();
+    const start = new Date(this.event.event_start_datetime);
+    const end = new Date(this.event.event_end_datetime);
+    this.isLive = start <= now && now < end;
   }
 
   private checkIfEventIsPassed(): void {
@@ -48,11 +58,9 @@ export class EventCardComponent implements OnInit, OnDestroy {
   }
 
   private getDuration(): void {
-    const today = new Date();
     const start = new Date(this.event.event_start_datetime);
     const end = new Date(this.event.event_end_datetime);
     const duration = getDuration(start, end);
-    this.isEventToCome = end >= today;
     this.duration = convertMinutesToHoursMinutes(duration);
   }
 
@@ -78,11 +86,15 @@ export class EventCardComponent implements OnInit, OnDestroy {
     });
   }
 
-  connect(event_static_url: string): void {
-    if (this.isUser) {
-      console.log('connect for user', event_static_url);
-    } else {
-      console.log('connect for participant', event_static_url);
+  connect(): void {
+    if (!this.isUser) {
+      const videoRehabApp = this.account.apps.find((app) => app.app_service_key === 'VideoRehabService');
+      console.log(videoRehabApp);
+      if (videoRehabApp && videoRehabApp.app_config) {
+        this.selectedSourceService.setSelectedSource(videoRehabApp.app_config.app_config_url);
+        const app = videoRehabApp.app_name?.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        this.router.navigate([Pages.createPath(Pages.appPage), {app}]);
+      }
     }
   }
 
